187cc8962fb09b0a3cb1309de285bab072464e4c
Toktomambetov-Sultan <sultan.toktomambetov.04@bk.ru>
2022-11-08 20:01:47 +0600

    for building

--- min.py
+++ min.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 import pyautogui as pg
-pg.FAILSAFE = False
+import autopy
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.mediapipe.python.solutions.hands
 hands = mpHands.Hands(min_detection_confidence=0.7)
@@ -16,17 +16,17 @@ while True:
 		for handLms in results.multi_hand_landmarks:
 			for id,lm in enumerate(handLms.landmark):
 				if id==0:
-					x = (lm.x - 0.4)/0.2
-					if lm.x<=0.4:
-						x = 0	
-					elif lm.x>=0.6:
-						x = 1	
-					y = (lm.y - 0.4)/0.2
-					if lm.x<=0.4:
-						x = 0	
-					elif lm.x>=0.6:
-						x = 1
-					pg.moveTo(ScreenSize.width*(1-x),ScreenSize.height*y,duration=0.05)
+					try:
+						autopy.mouse.move(ScreenSize.width*(1-lm.x),ScreenSize.height*lm.y)
+					except Exception as inst: 
+						print("Error: ", inst)
 			mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
-	cv2.imshow("Image",img)
+	cv2.imshow("Image",cv2.flip(img,1))
 	cv2.waitKey(1)
+
+# import handMatching 
+# a = handMatching()
+
+# def detect(landmarks): ...
+# a.apply(detect)
+# a.run()
